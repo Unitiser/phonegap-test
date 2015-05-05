ae94abed77b1e5655c0b24340f3a8afecff15ee7
Nicolas Cusson <ncusson@localhost.localdomain>
2015-05-05 16:03:51 -0400

    Implemented a small note taking app

--- www/app/note/note.js
+++ www/app/note/note.js
@@ -1,45 +1,60 @@
 angular.module('myApp').controller('NoteCtrl', function($scope, $http){
 
-	//Create/open note file
-	function onSuccess(fileSystem) {
-		var directoryReader = fileSystem.root.createReader();
-		directoryReader.readEntries(function(entries){
-			$scope.entries = entries;
-			for(var i = 0; i < entries.length; i++){
-				readChilds(entries[i]);
-			}
-			$scope.$apply();
-		},function(err){
-			$scope.err = err;
-		});
-		$scope.fsName = fileSystem.name;
-		$scope.fsRootPath = fileSystem.root.fullPath;
-		$scope.$apply();
-	
+	$scope.initFile = function(){
+		$scope.notes = [{
+			'title' : 'Hello world!',
+			'content' : 'This is the first entry...'
+		}];
+		$scope.saveNotes();
 	}
 
-	function readChilds(entry){
-		if(entry.isDirectory){
-			var reader = entry.createReader();
-			reader.readEntries(function(entries){
-				entry.childs = entries;
-				for(var i = 0; i < entries.length; i++){
-					readChilds(entries[i]);
+	$scope.readNotes = function(){
+		window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dir){
+			dir.getFile('notes.json', null, function(fileEntry){
+				var fileReader = new FileReader();
+				fileReader.onloadend = function(e){
+					var text = this.result;
+					console.log('File contains :', text);
+					if(text){
+						$scope.notes = JSON.parse(text);
+					}else{
+						$scope.notes = [];
+					}
+					$scope.$apply();
 				}
-				$scope.$apply();
-			}, function(err){
-				$scope.err = err;
+
+				fileEntry.file(function(file){
+					fileReader.readAsText(file);
+				});
 			});
-		}
+		});
+	};
+
+	$scope.saveNotes = function(){
+		window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dir){
+			dir.getFile('notes.json', {create: true}, function(fileEntry){
+				fileEntry.createWriter(function(fw){
+					fw.onwriteend = function() {
+					    if (fw.length === 0) {
+					        //fileWriter has been reset, write file
+					        fw.write(JSON.stringify($scope.notes));
+					    } else {
+					        //file has been overwritten with blob
+					        //use callback or resolve promise
+					    }
+					};
+					fw.truncate(0);
+				});
+			});
+		});
+	};
+
+	$scope.addNote = function(){
+		$scope.notes.push($scope.newNote);
+		$scope.newNote = {};
 	}
 
-	// request the persistent file system
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onSuccess, null);
-
-
-	//Parse note file
-	
-	//Save changes made to notes
+	$scope.readNotes();
 
 }).config(['$stateProvider', function($stateProvider) {
   $stateProvider
